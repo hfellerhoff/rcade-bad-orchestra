05619be7e3f478f7e25f25d51fece3d95d36a05f
hfellerhoff <henryfellerhoff@gmail.com>
2025-12-08 21:33:49 -0500

    now with twice the trombone

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import "./style.css";
-import { PLAYER_1, on, SYSTEM } from "@rcade/plugin-input-classic";
-import { PLAYER_1 as PLAYER_1_SPINNER } from "@rcade/plugin-input-spinners";
+import { PLAYER_1, on, SYSTEM, PLAYER_2 } from "@rcade/plugin-input-classic";
+import {
+  PLAYER_1 as PLAYER_1_SPINNER,
+  PLAYER_2 as PLAYER_2_SPINNER,
+} from "@rcade/plugin-input-spinners";
 import * as Tone from "tone";
+import { TROMBONE_URLS } from "./instruments";
 
 const overlay = document.querySelector<HTMLDivElement>("#overlay")!;
-const instrument = document.querySelector<HTMLDivElement>("#instrument")!;
+const instrument_1_element =
+  document.querySelector<HTMLDivElement>("#instrument-1")!;
+const instrument_2_element =
+  document.querySelector<HTMLDivElement>("#instrument-2")!;
 
 let gameStarted = false;
 
@@ -18,10 +25,31 @@ function mapToFrequencyLog(x: number): number {
   return MIN_NOTE * Math.pow(MAX_NOTE / MIN_NOTE, x);
 }
 
-let position = 50;
-let previousPosition = 0;
+function clampPositon(x: number) {
+  if (x < 0) {
+    return 0;
+  } else if (x > 100) {
+    return 100;
+  }
+  return x;
+}
 
-let trombone: Tone.Sampler | undefined = undefined;
+type PlayerState = {
+  instrument: Tone.Sampler | undefined;
+  position: number;
+  previousPosition: number;
+};
+
+const player_1_state: PlayerState = {
+  instrument: undefined,
+  position: 50,
+  previousPosition: 0,
+};
+const player_2_state: PlayerState = {
+  instrument: undefined,
+  position: 50,
+  previousPosition: 0,
+};
 
 function positionToFrequency(pos: number) {
   return mapToFrequencyLog(pos / 100);
@@ -29,84 +57,103 @@ function positionToFrequency(pos: number) {
 
 on("inputStart", (input) => {
   if (input.player === 1 && input.pressed && input.button === "A") {
-    instrument.classList.add("playing");
+    instrument_1_element.classList.add("playing");
+  }
+  if (input.player === 2 && input.pressed && input.button === "A") {
+    instrument_2_element.classList.add("playing");
   }
 });
 
 on("inputEnd", (input) => {
   if (input.player === 1 && !input.pressed && input.button === "A") {
-    instrument.classList.remove("playing");
-    const frequency = positionToFrequency(position);
-    trombone?.triggerRelease(frequency);
+    instrument_1_element.classList.remove("playing");
+    const frequency = positionToFrequency(player_1_state.position);
+    player_1_state.instrument?.triggerRelease(frequency);
+  }
+
+  if (input.player === 2 && !input.pressed && input.button === "A") {
+    instrument_2_element.classList.remove("playing");
+    const frequency = positionToFrequency(player_2_state.position);
+    player_2_state.instrument?.triggerRelease(frequency);
   }
 });
 
 function update() {
   if (!gameStarted) {
-    if (SYSTEM.ONE_PLAYER) {
+    if (SYSTEM.TWO_PLAYER) {
       gameStarted = true;
       Tone.start();
       overlay.remove();
 
-      trombone = new Tone.Sampler({
-        urls: {
-          "A#1": "/assets/trombone/As1.ogg",
-          "A#2": "/assets/trombone/As2.ogg",
-          "A#3": "/assets/trombone/As3.ogg",
-          C3: "/assets/trombone/C3.ogg",
-          C4: "/assets/trombone/C4.ogg",
-          "C#2": "/assets/trombone/Cs2.ogg",
-          "C#4": "/assets/trombone/Cs4.ogg",
-          D3: "/assets/trombone/D3.ogg",
-          D4: "/assets/trombone/D4.ogg",
-          "D#2": "/assets/trombone/Ds2.ogg",
-          "D#3": "/assets/trombone/Ds3.ogg",
-          "D#4": "/assets/trombone/Ds4.ogg",
-          F2: "/assets/trombone/F2.ogg",
-          F3: "/assets/trombone/F3.ogg",
-          F4: "/assets/trombone/F4.ogg",
-          "G#2": "/assets/trombone/Gs2.ogg",
-          "G#3": "/assets/trombone/Gs3.ogg",
-        },
+      player_1_state.instrument = new Tone.Sampler({
+        urls: TROMBONE_URLS,
         onerror: (err) => {
           console.log(err);
         },
         onload: () => {
           console.log("loaded!");
         },
+        volume: 0.5,
       }).toDestination();
 
-      console.log(trombone);
+      player_2_state.instrument = new Tone.Sampler({
+        urls: TROMBONE_URLS,
+        onerror: (err) => {
+          console.log(err);
+        },
+        onload: () => {
+          console.log("loaded!");
+        },
+        volume: 0.5,
+      }).toDestination();
     }
   } else {
-    previousPosition = position;
-    position +=
+    player_1_state.previousPosition = player_1_state.position;
+    player_2_state.previousPosition = player_2_state.position;
+
+    player_1_state.position +=
       (PLAYER_1_SPINNER.SPINNER.step_delta /
         PLAYER_1_SPINNER.SPINNER.step_resolution) *
       40;
+    player_1_state.position = clampPositon(player_1_state.position);
 
-    if (position < 0) {
-      position = 0;
-    } else if (position > 100) {
-      position = 100;
-    }
+    player_2_state.position +=
+      (PLAYER_2_SPINNER.SPINNER.step_delta /
+        PLAYER_2_SPINNER.SPINNER.step_resolution) *
+      40;
+    player_2_state.position = clampPositon(player_2_state.position);
 
-    if (PLAYER_1.A && !!trombone) {
-      const previousFrequency = positionToFrequency(previousPosition);
-      const currentFrequency = positionToFrequency(position);
+    if (PLAYER_1.A && !!player_1_state.instrument) {
+      const previousFrequency = positionToFrequency(
+        player_1_state.previousPosition,
+      );
+      const currentFrequency = positionToFrequency(player_1_state.position);
 
-      trombone.triggerAttack(currentFrequency);
+      player_1_state.instrument.triggerAttack(currentFrequency);
 
       // console.log(previousFrequency, currentFrequency);
-      if (position !== previousPosition) {
-        trombone?.triggerRelease(previousFrequency);
-        trombone?.triggerAttack(currentFrequency);
+      if (player_1_state.position !== player_1_state.previousPosition) {
+        player_1_state.instrument?.triggerRelease(previousFrequency);
+        player_1_state.instrument?.triggerAttack(currentFrequency);
+      }
+    }
+    if (PLAYER_2.A && !!player_2_state.instrument) {
+      const previousFrequency = positionToFrequency(
+        player_2_state.previousPosition,
+      );
+      const currentFrequency = positionToFrequency(player_2_state.position);
+
+      player_2_state.instrument.triggerAttack(currentFrequency);
+
+      // console.log(previousFrequency, currentFrequency);
+      if (player_2_state.position !== player_2_state.previousPosition) {
+        player_2_state.instrument?.triggerRelease(previousFrequency);
+        player_2_state.instrument?.triggerAttack(currentFrequency);
       }
     }
 
-    // const percentage = (position / (Math.PI * 2)) * 100;
-
-    instrument.style = `width: ${100 - position}%`;
+    instrument_1_element.style = `width: ${100 - player_1_state.position}%`;
+    instrument_2_element.style = `width: ${100 - player_2_state.position}%`;
   }
 
   requestAnimationFrame(update);
